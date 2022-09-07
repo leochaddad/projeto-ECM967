0b578fbf887bf8c9745972f1d84778aa0cdc7a60
Leonardo Haddad <leonardo.haddad@neofield.com.br>
2022-09-07 13:34:52 -0300

    resolvers

--- README.md
+++ README.md
@@ -1,5 +1,76 @@
 # Projeto ECM967 – Tópicos Avançados em Back End
 
-- Leonardo Cury Haddad
-- Matheus Lopes Vivas
-- Rodrigo Martins de Carvalho
+- Leonardo Cury Haddad 18.00442-3
+- Matheus Lopes Vivas 17.04401-4
+- Rodrigo Martins de Carvalho 16.03378-7
+
+## O sistema deve permitir as seguintes consultas (apenas operações do tipo Query):
+
+### Lista completa de usuários, incluindo seus posts, comentários e reações.
+
+```gql
+{
+  users {
+    posts {
+      id
+      text
+    }
+    comments {
+      id
+      text
+    }
+    reactions {
+      id
+      type
+    }
+  }
+}
+```
+
+### Lista completa de posts, incluindo seus comentários e reações
+
+```gql
+{
+  posts {
+    id
+    text
+    comments {
+      id
+      text
+    }
+    reactions {
+      id
+      type
+    }
+  }
+}
+```
+
+### Lista completa de comentários, incluindo seus autores.
+
+```gql
+{
+  comments {
+    text
+    author {
+      id
+      name
+    }
+  }
+}
+```
+
+### Uma consulta que mostra, dentro da lista completa de reações, o percentual de reações positivas e negativas.
+
+```gql
+{
+  allReactions {
+    likePercentage
+    dislikePercentage
+    reactions {
+      id
+      type
+    }
+  }
+}
+```
--- src/index.ts
+++ src/index.ts
@@ -16,16 +16,20 @@
 // - Lista completa de comentários, incluindo seus autores.
 // - Uma consulta que mostra, dentro da lista completa de reações, o percentual de
 
-
 import { createServer } from "graphql-yoga";
 import db from "../db";
-
 
 const typeDefs = /* GraphQL */ `
   type Query {
     users: [User!]!
     posts: [Post!]!
     comments: [Comment!]!
+    allReactions: AllReactions!
+  }
+
+  type AllReactions {
+    likePercentage: Float!
+    dislikePercentage: Float!
     reactions: [Reaction!]!
   }
 
@@ -48,6 +52,7 @@ const typeDefs = /* GraphQL */ `
   type Comment {
     id: ID!
     text: String!
+    author: User
   }
 
   type Reaction {
@@ -58,15 +63,35 @@ const typeDefs = /* GraphQL */ `
 
 const resolvers = {
   Query: {
-    users: () => db.users.map(user => ({
-      ...user,
-      posts: db.posts.filter(post => post.author === user.id),
-      comments: db.comments.filter(comment => comment.author === user.id),
-      reactions: db.reactions.filter(reaction => reaction. === user.id)
-    })),
-
-
-
+    users: () =>
+      db.users.map((user) => ({
+        ...user,
+        posts: db.posts.filter((post) => post.author === user.id),
+        comments: db.comments.filter((comment) => comment.author === user.id),
+        reactions: db.reactions.filter((reaction) => reaction.user === user.id),
+      })),
+    posts: () =>
+      db.posts.map((post) => ({
+        ...post,
+        comments: db.comments.filter((comment) => comment.post === post.id),
+        reactions: db.reactions.filter((reaction) => reaction.post === post.id),
+      })),
+    comments: () =>
+      db.comments.map((comment) => ({
+        ...comment,
+        author: db.users.find((user) => user.id === comment.author),
+      })),
+    allReactions: () => ({
+      likePercentage:
+        db.reactions.filter((reaction) => reaction.type === "LIKE").length /
+        db.reactions.length,
+      dislikePercentage:
+        db.reactions.filter((reaction) => reaction.type === "DISLIKE").length /
+        db.reactions.length,
+      reactions: db.reactions,
+    }),
+  },
+};
 
 const server = createServer({
   schema: {
